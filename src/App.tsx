8b6737c733c46094dbe985277b71ecb4f379afca
Victor Salles <victorgamesbr091@gmail.com>
2024-09-04 19:46:06 -0300

    ✨feat: criação da tela de notificações

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import './globalCss/index.css'
+import Notificacoes from './pages/Notificações'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/notificacoes' element={<Notificacoes />} />
       </Routes>
     </BrowserRouter>
   )
