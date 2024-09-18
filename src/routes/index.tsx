07ddb94fb4c0a4e6b57d0728267db0f3ae2d51dd
Pedro Henrique de Souza <ph9530338@gmail.com>
2024-09-18 09:00:36 -0300

    ✨ - feat: tela de login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import CadastroEstacao from "../pages/CadastroEstação/cadastroEstacao";
 import DropdownExample from "../pages/DropdownExample";
 import DropdownUsuario from "../pages/DropdownUsuario";
 import Notificacoes from "../pages/Notificações";
+import Login from "../pages/Login/login";
 import '../globalCss/index.css'
 import Home from "../pages/Home/home";
 import AprendaMais from "../pages/AprendaMais/aprendaMais";
@@ -11,7 +12,8 @@ export default function Rotas() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<Login />} />
+                <Route path='/home' element={<Home />} />
                 <Route path='/notificacoes' element={<Notificacoes />} />
                 <Route path='/estacao/cadastro' element={<CadastroEstacao />} />
                 <Route path='/dropdown-exemplo' element={<DropdownExample />} />
