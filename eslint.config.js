fbfd2f80578f5b1c32b64a8485e501a0a838b265
Laura Gabriel <eulauragabriel@gmail.com>
2024-11-06 01:12:42 -0300

    🐛fix: Corrigindo bugs no babel, pipeline e lint - R: 13

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from './node_modules/globals/globals.json' assert { type: 'json' };
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
+      parser: tsParser,
+      sourceType: 'module',
     },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -25,4 +29,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+];
